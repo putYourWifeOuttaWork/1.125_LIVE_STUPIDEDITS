c79c4a6437ff83987502dda8373c8dfd12c42e4a
putYourWifeOuttaWork <weisbergmm+1@gmail.com>
2025-11-19 19:56:45 -0500

    Updated 20251119200000_add_device_id_to_petri_observations.sql

--- supabase/migrations/20251119200000_add_device_id_to_petri_observations.sql
+++ supabase/migrations/20251119200000_add_device_id_to_petri_observations.sql
@@ -1,3 +1,4 @@
 Error: Failed to run sql query: ERROR: 42703: column s.device_id does not exist LINE 34: SET device_id = s.device_id ^ HINT: Perhaps you meant to reference the column "po.device_id".
 
 
+x
