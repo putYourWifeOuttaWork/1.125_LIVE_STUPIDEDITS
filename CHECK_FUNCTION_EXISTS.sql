4a9da5905e3a0302f8dc31e5ab7c88c4fcbc9c65
putYourWifeOuttaWork <weisbergmm+1@gmail.com>
2025-11-21 14:27:59 -0500

    Updated FORCE_RECREATE_FUNCTION.sql

--- CHECK_FUNCTION_EXISTS.sql
+++ CHECK_FUNCTION_EXISTS.sql
@@ -1,8 +1,11 @@
--- Check if the function exists and get its definition
-SELECT
+-- Check for ALL versions of this function
+SELECT 
   p.proname as function_name,
   pg_get_function_arguments(p.oid) as arguments,
-  pg_get_functiondef(p.oid) as full_definition
+  pg_get_function_result(p.oid) as return_type,
+  p.prosrc LIKE '%DATE_PART%' as has_old_code,
+  p.prosrc LIKE '%EXTRACT(EPOCH FROM%' as has_new_code,
+  LENGTH(p.prosrc) as code_length
 FROM pg_proc p
 JOIN pg_namespace n ON p.pronamespace = n.oid
 WHERE p.proname = 'generate_session_wake_snapshot'
